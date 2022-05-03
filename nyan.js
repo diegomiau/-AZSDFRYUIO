54e011add3c612891c257a96277a06eb150c153d
diegomiau <97712970+diegomiau@users.noreply.github.com>
2022-05-03 17:44:51 -0500

    Add files via upload

---
+++ nyan.js
@@ -0,0 +1,16 @@
+var GestName = document.getElementById("name1").value;
+names_of_people.push(GestName);
+names_of_people.sort();
+function searching()
+{
+var s= document.getElementById("s1").value;
+var found=0;
+var j;
+for(j=0; j<names_of_people.length; J++)
+{
+if(s==names_of_people[j]){
+found=found+1
+}
+}
+document.getElementById("p2").innerHTML="name found"+found+" time/s";
+console.log("found name "+found+" time/s");
